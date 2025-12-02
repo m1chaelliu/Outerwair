02dfe6531dcbd167ddee5a6aa9d01214224f1690
elliottchia <elliottchia@berkeley.edu>
2025-12-02 00:21:03 -0800

    add accessibility testing with axe

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Enable axe accessibility testing in development
+if (import.meta.env.DEV) {
+  import('react').then((React) => {
+    import('react-dom').then((ReactDOM) => {
+      import('@axe-core/react').then((axe) => {
+        axe.default(React, ReactDOM, 1000)
+      })
+    })
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
